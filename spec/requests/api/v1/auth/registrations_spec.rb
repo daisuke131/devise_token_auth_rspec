aed6057326b7a3f9c7e5f36b86fd09e044845226
daisuke <dada.00131@google.com>
2019-05-15 21:33:13 +0900

    ユーザー登録テスト

--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
-
+  describe "POST /api/v1/auth" do
+    subject { post(api_v1_user_registration_path, params: params) }
+    let(:params) { attributes_for(:user) }
+    it "ユーザー登録できる" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res["status"]).to eq("success")
+      expect(res["data"]["id"]).to eq(User.last.id)
+      expect(res["data"]["email"]).to eq(User.last.email)
+      expect(response).to have_http_status(200)
+    end
+  end
 end
